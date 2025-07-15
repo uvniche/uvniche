da472309bda0545c8c032a0ee6cb8961695a48ad
avaneesh <uvniche@gmail.com>
2025-07-15 20:35:30 +0530

    Center name and username below profile picture on all screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
-      <div className="flex items-center gap-6">
-        {/* Black profile picture with white border */}
+      <div className="flex flex-col items-center gap-6">
+        {/* Black profile picture */}
         <div className="w-20 h-20 rounded-full bg-black" />
         {/* Name and username */}
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
           <span className="text-2xl font-semibold text-white">avaneesh</span>
           <span className="text-lg text-white">@uvniche</span>
         </div>
