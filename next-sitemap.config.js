ca59b8ecc1065a71010b90ede730d7404d690318
avaneesh <uvniche@gmail.com>
2025-07-16 16:23:38 +0530

    feat: add next-sitemap setup, config, and generated sitemap/robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://www.uvniche.com', // Update this to your actual production URL
+  generateRobotsTxt: true, // (optional) Generate robots.txt
+  // ...other options can be added here
+}; 
