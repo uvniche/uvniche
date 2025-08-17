99bda66bfb9bfd7bcc2e1d0377f93c228a41b16d
avaneesh <uvniche@gmail.com>
2025-08-17 11:44:41 +0530

    Fix robots meta tag to allow search engine indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,12 @@
 import { MetadataRoute } from 'next'
+import { config } from '@/lib/config'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NODE_ENV === 'production' 
-    ? 'https://uvniche.com' 
-    : 'http://localhost:3000'
-
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: `${config.baseUrl}/sitemap.xml`,
   }
 }
