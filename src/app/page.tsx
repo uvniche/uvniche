826b8f457453a0eb8dd560998835c18ec1c44930
avaneesh <uvniche@gmail.com>
2025-08-17 12:15:36 +0530

    Fix syntax error in page.tsx import statement for Vercel build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-thisimport { Search } from "@/components/Search";
+import { Search } from "@/components/Search";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
