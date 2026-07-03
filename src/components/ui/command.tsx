7a8f4c592289a63b231a5b1624919b6c3302d75f
avaneesh <uvniche@gmail.com>
2026-07-03 15:21:41 +0530

    Prevent mobile input auto zoom

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -49,7 +49,7 @@ function CommandInput({
         ref={inputRef}
         data-slot="command-input"
         className={cn(
-          "placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
+          "placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-base outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         aria-autocomplete="list"
