28f9a4bbc35f9c617b4a06d57fee4db5551a54ae
avaneesh <uvniche@gmail.com>
2026-07-03 15:47:47 +0530

    Reduce search bar width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         </div>
 
         {/* Search Links Section */}
-        <div className="w-64 max-w-full">
+        <div className="w-56 max-w-full">
           <Search />
         </div>
       </div>
